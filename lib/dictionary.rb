291e1ce5153ea25342f2de5157f6b17c05028ab5
Blake <blameswalker@gmail.com>
2023-07-07 20:14:32 -0700

    Clarify Dictionary module objective(s) with overhead comment

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,3 +1,4 @@
+# Module Dictionary neatly bundles methods related to dictionary file interactions
 module Dictionary
   WORDS = File.open('google-10000-english-no-swears.txt', 'r').readlines.map{ |word| word.chomp }
 	.select{ |word| word.length.between?(5, 12) }
