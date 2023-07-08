401d573243e9980e1fa2a492d8baf512a2e6783d
Blake <blameswalker@gmail.com>
2023-07-08 15:21:24 -0700

    Rework word generation for Game objects; start work on guess method

--- lib/game.rb
+++ lib/game.rb
@@ -18,18 +18,6 @@ class Letter
   end
 end
 
-# Word objects will contain random strings pulled from google-10000-english-no-swears.txt
-class Word
-  def initialize(string)
-    @string = string
-    @letters = @string.split('').map { |letter| Letter.new(letter) }
-  end
-
-  def display
-    @letters.each { |letter| print "#{letter.display} "}
-  end
-end
-
 # Game objects will create new sets of game data (e.g. the word, remaining guesses, etc.)
 class Game
   attr_reader :word, :guesses_remaining, :used_letters
@@ -37,11 +25,19 @@ class Game
   include Dictionary
 
   def initialize
-    @word = random_word
+    @word = random_word.split('').map { |chr| Letter.new(chr) }
     @guesses_remaining = 6
     @used_letters = []
+  end
+
+  def guess(letter)
+    # has letter been used already?
+    # if no, check word for letter and change visibility of matches to true
+    # if yes, display error message and repeat method
   end
 end
 
 # tests
 game = Game.new
+puts game.word.string
+game.word.display
