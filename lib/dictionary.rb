d1e7fee243ba3f4abd09f246d8fa64ca43844c43
Blake <blameswalker@gmail.com>
2023-09-19 18:17:46 -0700

    Modularize Dictionary module to improve readability

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,7 +1,9 @@
 # Module Dictionary handles word generation from a 1000-word list
 module Dictionary
-  WORDS = File.readlines('google-10000-english-no-swears.txt').map { |word| word.chomp }
-              .select { |word| word.length.between?(5, 12) }
+  DICTIONARY_FILE = 'google-10000-english-no-swears.txt'
+
+  WORDS = File.readlines(DICTIONARY_FILE).map { |word| word.chomp }
+          .select { |word| word.length.between?(5, 12) }
 
   def random_word
     WORDS.sample
